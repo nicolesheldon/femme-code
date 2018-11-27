92fe1744084ced6d298824c0d20caf260da9a4cc
Nicole Sheldon <nws3@st-andrews.ac.uk>
2018-11-27 18:22:27 +0000

    javascript changes

--- homepage.js
+++ homepage.js
@@ -1,2 +1,4 @@
 
-alert("Hello, and welcome to our website!");
+function voteButton() {
+  alert("Thank you for voting! Currently 45% have voted for Michelle Obama and 55% for Malala Yousafza. ");
+}
